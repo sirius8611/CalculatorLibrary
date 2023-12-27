bca9902a60ee446f114dc5abd07e0d578db1ced5
sirius8611 <himymams@gmail.com>
2023-12-27 14:44:35 +0700

    Commit 7

--- calculator.py
+++ calculator.py
@@ -7,3 +7,5 @@ def subtract(first_term, second_term):
     return first_term - second_term
 def multiply(first_term, second_term):
     return first_term * second_term
+def division(first_term, second_term):
+    return first_term / second_term
--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,5 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+    def test_division(self):
+        assert 2 == calculator.division(100, 50)
