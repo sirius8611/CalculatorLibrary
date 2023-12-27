79b47995279c57d0b4877aaee1dab4e86fb21d38
sirius8611 <himymams@gmail.com>
2023-12-27 14:27:43 +0700

    test the system

--- test_calculator.py
+++ test_calculator.py
@@ -7,3 +7,5 @@ class TestCalculator:
         assert 4 == calculator.add(2, 2)
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
